66410b16acdf800499b2f5cdfad67d1f51408fb1
10744249 <61814396+10744249@users.noreply.github.com>
2024-03-16 21:50:05 +0800

    Update WebSecurityConfig.java

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+//前端請見資料夾front
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {

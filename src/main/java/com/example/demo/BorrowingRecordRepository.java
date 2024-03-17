f0c91a562807c92d6a708ca0f0a719ce6e26266b
10744249 <61814396+10744249@users.noreply.github.com>
2024-03-17 20:01:19 +0800

    Update BorrowingRecordRepository.java

--- src/main/java/com/example/demo/BorrowingRecordRepository.java
+++ src/main/java/com/example/demo/BorrowingRecordRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 @Repository
 public interface BorrowingRecordRepository extends JpaRepository<BorrowingRecord, Long> {
-    // 新增方法以便根據用戶ID和庫存ID查找特定的借閱記錄
+
     Optional<BorrowingRecord> findByUserIdAndInventoryId(Long userId, Long inventoryId);
 
     // 0316
